6afcd566a245794c2eeefd4f5f0e11796111f913
kinzang24 <gangagurung01791@gmail.com>
2026-09-19 16:10:39 +0800

    Initial commit of website project

--- OneDrive/Desktop/website/sisters-style-nextjs/sisters-style/app/api/config/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { STRIPE_ENABLED } from "@/lib/payments";
-
-export async function GET() {
-  return NextResponse.json({ stripeEnabled: STRIPE_ENABLED });
-}
